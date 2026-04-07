d6608ec23daad213fc62e8992e53105748b8b794
Your Name <your.email@example.com>
2026-04-07 06:49:59 +0530

    read data from db as well

---
+++ tests/e2eFlowDbData.spec.js
@@ -0,0 +1,73 @@
+const {test,expect} = require('@playwright/test')
+const getUserTestData = require('../utility/dbHelper')
+const ApiHelper = require('../utility/apiHelper')
+
+let userData,apiHelper
+
+test.beforeEach(async ({request})=>{
+    userData = await getUserTestData(1)
+    apiHelper = new ApiHelper(request)
+})
+test('reqres e2e flow', async function({request}){
+    //Register
+    const registerResp = await apiHelper.registerUser({
+        "email": userData.email,
+        "password": userData.register_password
+    })
+    const registerJson = await registerResp.json()
+    const token = registerJson.token
+    expect(token).not.toBeNull()
+    expect(registerResp.status()).toBe(200)
+    console.log(`Registered successfully | Token : ${registerJson.token}`)
+
+    // Login
+    const loginResp = await apiHelper.loginUser({
+        "email": userData.email,
+        "password": userData.login_password
+    })
+    const loginJson = await  loginResp.json()
+    expect(loginResp.status()).toBe(200)
+    expect(loginJson.token).not.toBeNull()
+    console.log(`Logged in | Token : ${loginJson.token}`)
+
+    //create a new user
+    const createResp = await apiHelper.newUser({
+        "name": userData.new_user_name,
+        "job": userData.new_user_job
+    })
+    const createJson = await createResp.json()
+    const userId = createJson.id
+    expect(createResp.status()).toBe(201)
+    expect(createJson.id).not.toBeNull()
+    console.log(`User created | id: ${createJson.id}`)
+
+    //Full update
+    const putResp = await apiHelper.fullUpdateUserDetails({
+        "name": userData.updated_name,
+        "job": userData.updated_job
+    },userId)
+    const putJson = await putResp.json()
+    expect(putResp.status()).toBe(200)
+    expect(putJson.name).toBe(userData.updated_name)
+    expect(putJson.job).toBe(userData.updated_job)
+    console.log(`Full update done | Name: ${putJson.name}`)
+
+    //partial update user details
+    const patchResp = await apiHelper.partialUpdateUserDetails({
+        "job": userData.partial_job
+    },userId)
+    const patchJson = await patchResp.json()
+    expect(patchResp.status()).toBe(200)
+    expect(patchJson.job).toBe(userData.partial_job)
+    console.log(`Partial update done | Job:${patchJson.job}`)
+
+    //delete the user
+    const deleteResp = await apiHelper.deleteUserDetails(userId)
+    expect(deleteResp.status()).toBe(204)
+    console.log(`User deleted | ID:${userId}`)
+
+    //Verify 404 after delete
+    const getResp = await  apiHelper.getUserDetailsPostDelete(userId)
+    expect(getResp.status()).toBe(404)
+    console.log(`404 found for User | ID: ${userId}`)
+})
---
+++ utility/dbHelper.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql2/promise')
+const dotenv = require('dotenv')
+dotenv.config({path:'./.env'})
+
+async function getConnection(){
+    return await mysql.createConnection({   
+        host : process.env.DB_HOST,
+        port : process.env.DB_PORT || 3306,
+        user : process.env.DB_USER,
+        password : (process.env.DB_PASSWORD).trim() || '',
+        database : process.env.DB_NAME_API
+    })
+}
+
+async function getUserTestData(id = 1) {
+    const connection = await getConnection()
+    try {
+        const [rows] = await connection.execute(`Select * from api_test_data where id = ?`, [id]);
+        if (rows.length === 0)
+            throw new Error(`No rows found for id= ${id}`)
+        return rows[0]
+    }
+    finally {
+        await connection.end()
+    }
+}
+
+module.exports = getUserTestData
